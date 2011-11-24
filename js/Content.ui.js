2ac01455740a2c4e5de1fd4fb24a0146159feeb3
discoverygnoye <gordie@discoverygarden.ca>
2011-11-24 09:03:02 -0400

    COLORADO-1688 Shrunk the search area to accomodate the Add Collection button
    The Add Collection button increased the width of the collection display header to
    the point where buttons were being lost over the right-hand edge of the screen.  To
    accomodate the new button, the width of the search text area is slightly reduced.

--- js/Content.ui.js
+++ js/Content.ui.js
@@ -96,11 +96,11 @@ ContentUi = Ext.extend(Ext.Panel, {
                 },
                 {
                     xtype: 'tbspacer',
-                    width: 10
+                    width: 5
                 },
                 {
                     xtype: 'trigger',
-                    width: 300,
+                    width: 280,
                     triggerClass: 'x-form-search-trigger',
                     id: 'adr-content-search-text'
                 },
